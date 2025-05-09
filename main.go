9e25a58276408711af479016c789c0d33121cb9d
afifah naufal rahmani <afifahrahmani466@gmail.com>
2025-05-09 21:42:47 +0700

    emot lagi

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm/logger"
 )
 
 func main() {
 	app := fiber.New()
-	
+
 	app.Use(logger.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -15,21 +16,20 @@ func main() {
 
 	// Tambahan untuk commit ke-2
 	app.Get("/about", func(c *fiber.Ctx) error {
-		return c.SendString("Ini halaman about!")
+		return c.SendString("ℹ️ Ini halaman about! Terima kasih sudah mengunjungi.")
 	})
-
 	// route baru
 	app.Get("/hello/:name", func(c *fiber.Ctx) error {
 		name := c.Params("name")
 		return c.SendString("Hello, " + name + "!")
 	})
 	// respon JSON
-    app.Get("/data", func(c *fiber.Ctx) error {
-        return c.JSON(fiber.Map{
-            "status":  "success",
-            "message": "Ini response JSON",
-        })
-    })
+	app.Get("/data", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status":  "success",
+			"message": "Ini response JSON",
+		})
+	})
 
 	app.Listen(":3000")
 }
