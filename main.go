0bdee7f6958bd79525fbfc6c16bfe5ea65983522
afifah naufal rahmani <afifahrahmani466@gmail.com>
2025-05-09 21:11:43 +0700

    nambahin kalimat

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("Hello, World!" "selamat datang di halaman ini!!" )
+		
 	})
 
 	app.Listen(":3000")
