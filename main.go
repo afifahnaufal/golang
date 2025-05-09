599a2a148e38c9637915a045ef6769ab17a5ee9e
afifah naufal rahmani <afifahrahmani466@gmail.com>
2025-05-09 21:23:31 +0700

    tambah route baru

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!" "selamat datang di halaman ini!!" 
-		"ini halaman tutorial" )
-		
+		return c.SendString("Hello, World! Selamat datang di halaman ini!! Ini halaman tutorial")
+	})
+
+	// Tambahan untuk commit ke-2
+	app.Get("/about", func(c *fiber.Ctx) error {
+		return c.SendString("Ini halaman about!")
 	})
 
 	app.Listen(":3000")
