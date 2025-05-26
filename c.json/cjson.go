0feb9a398f6a663e28f3f072a302e17b5ffb686a
afifah naufal rahmani <afifahrahmani466@gmail.com>
2025-05-26 19:51:22 +0700

    .

---
+++ c.json/cjson.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+type User struct {
+    ID    int    `json:"id"`
+    Name  string `json:"name"`
+    Email string `json:"email"`
+}
+
+func getUser(c *gin.Context) {
+    user := User{
+        ID:    1,
+        Name:  "Afifah",
+        Email: "afifah@example.com",
+    }
+    c.JSON(http.StatusOK, user)
+}
+
+func main() {
+    r := gin.Default()
+    r.GET("/user", getUser)
+    r.Run() // default di :8080
+}
