93d089944558d2dc3d902b218917f8c085d1ecaa
afifah naufal rahmani <afifahrahmani466@gmail.com>
2025-05-26 22:53:54 +0700

    text

--- c.json/cjson.go
+++ c.json/cjson.go
@@ -19,7 +19,7 @@ type User struct {
 func getUser() User {
     return User{
         ID:          1,
-        Name:        "Afifah Naufal Rahman",
+        Name:        "Afifah Naufal Rahma",
         Email:       "afifah@example.com",
         Status:      "Mahasiswa",
         Universitas: "Logistik dan Bisnis International",
